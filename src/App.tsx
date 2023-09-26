8e3312045966ef3f8a5cfeddbd3e2ded9529f1ca
Olayinka Ogunwemimo <mimoyinka@gmail.com>
2023-09-26 22:03:17 +0100

    PersonalInfo Reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { ChangeEvent, FormEventHandler } from 'react';
 import './App.css';
+import {
+  initialState,
+  personalInfoReducer,
+  toggleInternalUse,
+  toggleShow
+} from './reducers/personalInfo'
+import SimpleField from './components/SimpleField';
+import Question from './components/Question';
+import withToggle from './components/withToggle';
+
+interface  Props {
+  onSave: (arg: QuestionConfig) => void
+}
+const QuestionToggle = withToggle<Props>(Question)
 
 function App() {
+  const [personalInfo, dispatch] = React.useReducer(personalInfoReducer, initialState)
+
+  const delegateChangeHandler = ({ target: { name, parentElement } }: ChangeEvent<HTMLInputElement>) => {
+    if (!parentElement) return;
+    const field = parentElement.getAttribute("data-target")
+
+    if (field) {
+      if (name === "internalUse") {
+        dispatch(toggleInternalUse(field as keyof PersonalInfoConfig))
+      } else if (name === "show") {
+        dispatch(toggleShow(field as keyof PersonalInfoConfig))
+      }
+    }
+  }
+
+  const handleQuestion = (payload) => {
+    dispatch({
+      type: 'add_question',
+      payload
+    })
+  }
+
+  const fieldsArr = Object.keys(personalInfo)
+  // console.log(personalInfo)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul onChange={delegateChangeHandler as FormEventHandler}>
+        {
+          fieldsArr.map(field => <li key={field} data-target={field}>
+            {
+              field === "personalQuestions" ?
+                personalInfo[field].map(({question}) => <span>{question}</span>)
+                :
+                <SimpleField
+                  label={field}
+                  internalUse={personalInfo[field].internalUse}
+                  show={personalInfo[field].show}
+                />
+            }
+          </li>)
+        }
+      </ul>
+      <QuestionToggle onSave={handleQuestion}/>
     </div>
   );
 }
---
+++ src/reducers/personalInfo.ts
@@ -0,0 +1,56 @@
+import { Reducer } from 'react';
+
+const defaultFieldConfig = {
+    "internalUse": false,
+    "show": true
+}
+export const initialState = {
+    "firstName": { ...defaultFieldConfig },
+    "lastName": { ...defaultFieldConfig },
+    "emailId": { ...defaultFieldConfig },
+    "phoneNumber": { ...defaultFieldConfig },
+    "nationality": { ...defaultFieldConfig },
+    "currentResidence": { ...defaultFieldConfig },
+    "idNumber": { ...defaultFieldConfig },
+    "dateOfBirth": { ...defaultFieldConfig },
+    "gender": { ...defaultFieldConfig },
+    "personalQuestions": []
+}
+export const personalInfoReducer: Reducer<PersonalInfoConfig, BasicAction | QuestionAction> = (state, action) => {
+    function toggle<T, K extends keyof T>(obj: T, key: K) {
+        return { ...obj, [key]: !obj[key] }
+    }
+    switch (action.type) {
+        case 'toggle':
+            const { field, config } = action as BasicAction
+            const objPath = state[field] as FieldConfig
+            return {
+                ...state,
+                [field]: toggle<FieldConfig, typeof config>(objPath, config)
+            }
+            case 'add_question':
+                const { payload } = action as QuestionAction
+                return {
+                    ...state,
+                    personalQuestions: state.personalQuestions.concat(payload)
+                } 
+        default:
+            return state
+    }
+}
+
+export function toggleInternalUse(field: keyof PersonalInfoConfig): BasicAction {
+    return {
+        type: 'toggle',
+        config: 'internalUse',
+        field
+    }
+}
+
+export function toggleShow(field: keyof PersonalInfoConfig): BasicAction {
+    return {
+        type: 'toggle',
+        config: 'show',
+        field
+    }
+}
