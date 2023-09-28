0535c1e5ea6c9e3c204400194ffeb025eff89a5b
Olayinka Ogunwemimo <mimoyinka@gmail.com>
2023-09-28 20:25:06 +0100

    Refactor: Use antd Switch and Checkbox

--- src/components/SimpleField.tsx
+++ src/components/SimpleField.tsx
@@ -1,35 +1,55 @@
-import React from "react"
+import React, { useState } from "react"
+import { Switch } from 'antd';
 import { PERSONAL_INFO_LABELS } from "../constants";
+import { Checkbox } from 'antd';
+import type { CheckboxChangeEvent } from 'antd/es/checkbox';
 
 
-const SimpleField = ({ label, detail: {internalUse, show }}) => {
-  // if(label === 'gender') console.log(show)
+interface Props {
+  label: string,
+  detail: any,
+  toggleShow: (arg:string) => void,
+  toggleInternalUse?: (arg:string) => void
+  toggleMandatory?: (arg:string) => void
+}
+
+const SimpleField: React.FC<Props> = ({ label, detail, toggleShow, toggleInternalUse, toggleMandatory }) => {
   const title = PERSONAL_INFO_LABELS[label]?.title || label;
   const subtitle = PERSONAL_INFO_LABELS[label]?.subtitle;
+
+  const [switchState, setSwitchState] = useState<boolean>(detail.show)
+  const [checkState, setCheckState] = useState<boolean>(detail.internalUse || detail.mandatory)
+
+  const onSwitchChange = () => {
+    setSwitchState(!switchState)
+    toggleShow(label)
+  }
+
+  const onCheckboxChange = (e: CheckboxChangeEvent) => {
+    if(toggleInternalUse) {
+      setCheckState(!checkState)
+      toggleInternalUse(label);
+      return;
+    }
+    if(toggleMandatory) {
+      setCheckState(!checkState)
+      toggleMandatory(label)
+    }
+  };
+
 
     return <li className='Field-wrapper'>
       <span className="Field">
         {title}
         {subtitle ? <span className="Field-subtitle"> ({subtitle})</span> : null}
         </span>
-      <label htmlFor="internalUse">
-        <input
-          data-target={label}
-          type='checkbox'
-          name="internalUse"
-          defaultChecked={internalUse}
-        />
-        Internal
-      </label>
+        <Checkbox checked={checkState} onChange={onCheckboxChange}>
+          { toggleInternalUse ? 'Internal' : 'Mandatory'}
+          </Checkbox>
 
-      <label htmlFor="" key={"show_" + show}>
-        <input
-          data-target={label}
-          type='checkbox'
-          name="show"
-          defaultChecked={show}
-        />
-        {show ? 'Show' : 'Hide'}
+      <label htmlFor="">
+        <Switch checked={switchState} data-target={label} onChange={onSwitchChange}/>
+        <span className="Switch-label">{switchState ? 'Show' : 'Hide'}</span>
       </label>
     </li>
   }
